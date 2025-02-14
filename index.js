1acccbe0099539599126101e0f57c4515be3f7ed
sikwan <yysskk99999@naver.com>
2025-02-14 16:43:12 +0900

    mongoDB env manager

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
+const {User} = require('./models/User')
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 console.log('서버 시작');
 
@@ -20,8 +26,29 @@ mongoose.connect('mongodb+srv://yangsikwan:abcd1234@cluster0.c5scg.mongodb.net/?
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World ㅎㅇㅇ');
+    res.send('Hello World ㅎㅇㅇnodemon add');
 });
+
+
+app.post('/register', (req, res) => {
+    //회원가입시 필요한 정보들 클라이언트에서 받아오기
+    //그것들을 데이터베이스에 넣어준다
+
+    const user = new User(req.body)
+
+
+    user.save()
+        .then(userInfo => {
+            res.status(200).json({
+                success: true
+            })
+        })
+        .catch(err => {
+            res.json({ success: false, err })
+        })
+
+})
+
 
 app.listen(port, () => {
     console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
