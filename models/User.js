52d517019141c37ae94e8e2d69b6cb4a49612c1d
sikwan <yysskk99999@naver.com>
2025-02-14 16:58:38 +0900

    bcrypt add

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -34,7 +35,18 @@ const userSchema = mongoose.Schema({
 })
 userSchema.pre('save',function(next){
 
+    var user = this;
+bcrypt.genSalt(saltRounds,function(err,salt){
+    if(err) return next(err)
+    bcrypt.hash(user.password,salt,function(err,hash){
+        if(err) return next(err)
+        user.password = hash
+        next()
+    })
+
 })
+})
+
 
 const User = mongoose.model('User', userSchema)
 
